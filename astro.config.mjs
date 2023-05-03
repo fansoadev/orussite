800807bd9cd5e1713361a7ac46d58ce2d808a3cc
Ezdev2Pro <ezra.fanomezantsoa@myagency.mg>
2023-05-03 18:25:37 +0300

    add astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import astroI18next from "astro-i18next";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://orussite.github.io",
+  base: "/orussite",
   integrations: [tailwind(), astroI18next()],
   vite: {
     ssr: {
