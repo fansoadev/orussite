c7eb6bd60580f95389941d4f120fbd6e49425226
Ezdev2Pro <ezra.fanomezantsoa@myagency.mg>
2023-05-03 18:40:19 +0300

    add cname

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import astroI18next from "astro-i18next";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://sub.orussite.com/",
+  site: "https://sub.orussite.com",
   base: "/orussite",
   integrations: [tailwind(), astroI18next()],
   vite: {
